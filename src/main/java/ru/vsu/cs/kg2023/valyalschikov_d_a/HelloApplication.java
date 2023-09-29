9f4bea72b87978b654b1e35931637afa61611201
valyalshikovd <770vda@mail.ru>
2023-09-29 23:40:16 +0300

    Создаю базовое приложение

---
+++ src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/HelloApplication.java
@@ -0,0 +1,23 @@
+package ru.vsu.cs.kg2023.valyalschikov_d_a;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainwindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Proto Curve App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
