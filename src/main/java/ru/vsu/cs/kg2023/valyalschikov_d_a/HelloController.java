b7985ab3a2129337950a080ee3d1c8121cbaec91
valyalshikovd <770vda@mail.ru>
2023-10-04 16:15:19 +0300

    добавлен коэффициент определяющий зависимость от параметра

--- src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/HelloController.java
+++ src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/HelloController.java
@@ -6,8 +6,11 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import ru.vsu.cs.kg2023.valyalschikov_d_a.spline.PointForSpline;
+import ru.vsu.cs.kg2023.valyalschikov_d_a.spline.Spline;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
 
@@ -16,7 +19,11 @@ public class HelloController {
     @FXML
     private Canvas canvas;
 
-    ArrayList<Point2D> points = new ArrayList<Point2D>();
+    ArrayList<PointForSpline> pointsX = new ArrayList<PointForSpline>();
+    ArrayList<PointForSpline> pointsY = new ArrayList<PointForSpline>();
+    List<PointForSpline> lastPoints = new ArrayList<>();
+    int counterOfPoints = 0;
+    double c = 0.2;
 
     @FXML
     private void initialize() {
@@ -33,15 +40,76 @@ public class HelloController {
     private void handlePrimaryClick(GraphicsContext graphicsContext, MouseEvent event) {
         final Point2D clickPoint = new Point2D(event.getX(), event.getY());
 
-        final int POINT_RADIUS = 3;
-        graphicsContext.fillOval(
-                clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS,
-                2 * POINT_RADIUS, 2 * POINT_RADIUS);
-
-        if (points.size() > 0) {
-            final Point2D lastPoint = points.get(points.size() - 1);
-            graphicsContext.strokeLine(lastPoint.getX(), lastPoint.getY(), clickPoint.getX(), clickPoint.getY());
+        if(counterOfPoints == 0){
+            System.out.println("-");
+            pointsX.add(new PointForSpline(0, clickPoint.getX()));
+            pointsY.add(new PointForSpline(0, clickPoint.getY()));
+            counterOfPoints++;
+            drawSpline(graphicsContext);
+            return;
         }
-        points.add(clickPoint);
+
+        if (counterOfPoints == 1) {
+            System.out.println("--");
+            double params = pointsX.get(0).getCoordParameter() + Math.pow(Math.pow(clickPoint.getX() + pointsX.get(counterOfPoints - 1).getVal(), 2) +
+                    Math.pow(clickPoint.getY() - pointsY.get(counterOfPoints - 1).getVal(), 2), 0.5);
+            PointForSpline ptX = new PointForSpline(params*c, clickPoint.getX());
+            PointForSpline ptY = new PointForSpline(params*c, clickPoint.getY());
+            ptX.setFirstDiff((ptX.getVal() - pointsX.get(0).getVal()) /
+                    (ptX.getCoordParameter() - pointsX.get(0).getCoordParameter()));
+            ptY.setFirstDiff((ptY.getVal() - pointsY.get(0).getVal()) /
+                    (ptY.getCoordParameter() - pointsY.get(0).getCoordParameter()));
+            ptX.setSecondDiff(0);
+            ptY.setSecondDiff(0);
+            pointsX.add(ptX);
+            pointsY.add(ptY);
+            counterOfPoints++;
+            drawSpline(graphicsContext);
+            return;
+        }
+        double params = pointsX.get(counterOfPoints-2).getCoordParameter() + Math.pow(Math.pow(clickPoint.getX() + pointsX.get(counterOfPoints - 1).getVal(), 2) +
+                Math.pow(clickPoint.getY() - pointsY.get(counterOfPoints - 1).getVal(), 2), 0.5);
+        PointForSpline ptX = new PointForSpline(params*c, clickPoint.getX());
+        PointForSpline ptY = new PointForSpline(params*c, clickPoint.getY());
+        ptX.setFirstDiff((ptX.getVal() - pointsX.get(counterOfPoints-2).getVal()) /
+                (ptX.getCoordParameter() - pointsX.get(counterOfPoints-2).getCoordParameter()));
+        ptY.setFirstDiff((ptY.getVal() - pointsY.get(counterOfPoints-2).getVal()) /
+                (ptY.getCoordParameter() - pointsY.get(counterOfPoints-2).getCoordParameter()));
+        pointsX.add(ptX);
+        pointsY.add(ptY);
+
+        counterOfPoints++;
+//        System.out.println(Arrays.toString(pointsX.toArray()));
+//        System.out.println(Arrays.toString(pointsY.toArray()));
+        System.out.println("---");
+        drawSpline(graphicsContext);
+
+    }
+
+    private void drawSpline(GraphicsContext graphicsContext){
+        graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        final int POINT_RADIUS = 3;
+        for(int i = 0; i < counterOfPoints; i++){
+            graphicsContext.fillOval(
+                    pointsX.get(i).getVal() - POINT_RADIUS, pointsY.get(i).getVal(),
+                    2 * POINT_RADIUS, 2 * POINT_RADIUS);
+        }
+        if(counterOfPoints == 2){
+            graphicsContext.strokeLine(pointsX.get(0).getVal(), pointsY.get(0).getVal(), pointsX.get(1).getVal(), pointsY.get(1).getVal());
+            return;
+        }
+        if(counterOfPoints > 2) {
+            System.out.println("отрисовка сплайна");
+            Spline splineX = new Spline(pointsX, true);
+            Spline splineY = new Spline(pointsY, false);
+            List<Double> ptsX = splineX.getSpline();
+            List<Double> ptsY = splineY.getSpline();
+         //   System.out.println("Иксы" + Arrays.toString(ptsX.toArray()));
+           // System.out.println("Игрики" +Arrays.toString(ptsY.toArray()));
+            for (int i = 1; i < ptsX.size(); i++) {
+           //     System.out.println("------");
+                graphicsContext.strokeLine(ptsX.get(i - 1), ptsY.get(i - 1), ptsX.get(i), ptsY.get(i));
+            }
+        }
     }
 }
--- src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/Spline.java
+++ src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/Spline.java
@@ -13,8 +13,11 @@ public class Spline {
         return spline;
     }
 
-    public Spline(List<PointForSpline> val) {
+    public boolean isX;
+
+    public Spline(List<PointForSpline> val, boolean isX) {
         this.val = val;
+        this.isX = isX;
 
         getResultats();
 
@@ -25,26 +28,27 @@ public class Spline {
     private void getResultats() {
         for (int i = 2; i < val.size(); i++) {
             double[] coefficients = solutionEquation(val.get(i-2), val.get(i-1), val.get(i));
-          //  System.out.println("коэфф " +Arrays.toString(coefficients));
+            System.out.println(val.get(i - 1).getCoordParameter() + " Параметр точки -1");
+            System.out.println(val.get(i - 2).getCoordParameter() + " Параметр точки -2");
             double length = val.get(i - 1).getCoordParameter() - val.get(i - 2).getCoordParameter();
             double x = val.get(i - 2).getCoordParameter();
-            //System.out.println(length + "длинна");
-            System.out.println(val.get(i - 1).getCoordParameter());
-            for (int j = 0; j < length; j++) {
+            System.out.println(length + "длинна");
+            //System.out.println(val.get(i - 1).getCoordParameter());
+            for (int j = 0; j < Math.abs(length); j++) {
               //  System.out.println(length);
          //       System.out.println(j);
                 x ++;
                // System.out.println(x);
                 spline.add(coefficients[0] * Math.pow(x, 3) +
                         coefficients[1] * Math.pow(x, 2) +
-                                coefficients[2] * x +
+                                coefficients[2] * x+
                         coefficients[3]
                         );
             }
             if(i == val.size() - 1){
                 length = val.get(i ).getCoordParameter() - val.get(i -1).getCoordParameter();
                 x = val.get(i - 1).getCoordParameter();
-                for (int k = 0; k < length; k++) {
+                for (int k = 0; k < Math.abs(length) ; k++) {
                     x++;
                     spline.add(coefficients[0] * Math.pow(x, 3) +
                             coefficients[1] * Math.pow(x, 2)+
@@ -68,7 +72,7 @@ public class Spline {
                 {Math.pow(x2, 3), Math.pow(x2, 2), x2, 1},
                 {Math.pow(x3, 3), Math.pow(x3, 2), x3, 1},
                 {3 * x1 * x1, 2 * x1, 1, 0},
-             //   {6 * x2, 4, 0, 0}
+            //    {6 * x1, 4, 0, 0}
         });
         RealVector constants = new ArrayRealVector(
                 new double[]{pt1.getVal(), pt2.getVal(), pt3.getVal(), pt1.getFirstDiff()});
@@ -78,8 +82,8 @@ public class Spline {
         pt2.setFirstDiff(
                 3 * x2 * x2 * res[0] + 2 * x2  * res[1] + res[2]
         );
-        pt3.setSecondDiff(
-                6 * x3  * res[0] + 4  * res[1]
+        pt2.setSecondDiff(
+                6 * x2  * res[0] + 4  * res[1]
         );
         return solution.toArray();
 
