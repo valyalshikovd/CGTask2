2e22948a592ba2b24dbdb316cbfa3bc24c163dc1
valyalshikovd <770vda@mail.ru>
2023-10-04 14:43:14 +0300

    переработана структура, сплайн задан параметрически

--- src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/PointForSpline.java
+++ src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/PointForSpline.java
@@ -1,15 +1,41 @@
 package ru.vsu.cs.kg2023.valyalschikov_d_a.spline;
-import javafx.geometry.Point2D;
-public class PointForSpline extends Point2D {
+
+public class PointForSpline  {
     private double firstDiff;
+
+    public double getSecondDiff() {
+        return secondDiff;
+    }
+
+    public void setSecondDiff(double secondDiff) {
+        this.secondDiff = secondDiff;
+    }
+
+    private double secondDiff;
+    private double param;
+    private double val;
+
+    public double getCoordParameter() {
+        return param;
+    }
+
+    public double getVal() {
+        return val;
+    }
+
+    public void setCoordParameter(double coordParameter) {
+        this.param = coordParameter;
+    }
+
     public double getFirstDiff() {
         return firstDiff;
     }
     public void setFirstDiff(double firstDiff) {
         this.firstDiff = firstDiff;
     }
-    public PointForSpline(double var1, double var2) {
-        super(var1,  var2);
+    public PointForSpline(double param, double val) {
+        this.param = param;
+        this.val = val;
         firstDiff = 0;
     }
     @Override
--- src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/Spline.java
+++ src/main/java/ru/vsu/cs/kg2023/valyalschikov_d_a/spline/Spline.java
@@ -1,63 +1,87 @@
 package ru.vsu.cs.kg2023.valyalschikov_d_a.spline;
+
 import org.apache.commons.math3.linear.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Spline {
-    private final PointForSpline firstPoint;
-    private final PointForSpline secondPoint;
-    private final PointForSpline thirdPoint;
-    private final double x1;
-    private final double x2;
-    private final double x3;
-    private final double y1;
-    private final double y2;
-    private final double y3;
-    private double a;
-    private double b;
-    private double c;
-    private double d;
-    public Spline(PointForSpline firstPoint, PointForSpline secondPoint, PointForSpline thirdPoint) {
-        this.firstPoint = firstPoint;
-        this.secondPoint = secondPoint;
-        this.thirdPoint = thirdPoint;
-        this.x1 = firstPoint.getX();
-        this.x2 = secondPoint.getX();
-        this.x3 = thirdPoint.getX();
-        this.y1 = firstPoint.getY();
-        this.y2 = secondPoint.getY();
-        this.y3 = thirdPoint.getY();
-        solutionEquation();
-        secondPoint.setFirstDiff(3*a * Math.pow(secondPoint.getX(), 2) + 2*b * secondPoint.getX() + c);
+    private List<PointForSpline> val;
+    private List<Double> spline = new ArrayList<Double>();
+
+    public List<Double> getSpline() {
+        return spline;
     }
-    private void solutionEquation(){
+
+    public Spline(List<PointForSpline> val) {
+        this.val = val;
+
+        getResultats();
+
+    }
+
+
+
+    private void getResultats() {
+        for (int i = 2; i < val.size(); i++) {
+            double[] coefficients = solutionEquation(val.get(i-2), val.get(i-1), val.get(i));
+          //  System.out.println("коэфф " +Arrays.toString(coefficients));
+            double length = val.get(i - 1).getCoordParameter() - val.get(i - 2).getCoordParameter();
+            double x = val.get(i - 2).getCoordParameter();
+            //System.out.println(length + "длинна");
+            System.out.println(val.get(i - 1).getCoordParameter());
+            for (int j = 0; j < length; j++) {
+              //  System.out.println(length);
+         //       System.out.println(j);
+                x ++;
+               // System.out.println(x);
+                spline.add(coefficients[0] * Math.pow(x, 3) +
+                        coefficients[1] * Math.pow(x, 2) +
+                                coefficients[2] * x +
+                        coefficients[3]
+                        );
+            }
+            if(i == val.size() - 1){
+                length = val.get(i ).getCoordParameter() - val.get(i -1).getCoordParameter();
+                x = val.get(i - 1).getCoordParameter();
+                for (int k = 0; k < length; k++) {
+                    x++;
+                    spline.add(coefficients[0] * Math.pow(x, 3) +
+                            coefficients[1] * Math.pow(x, 2)+
+                            coefficients[2] * x  +
+                            coefficients[3]
+                    );
+                }
+            }
+
+        }
+
+    }
+
+    private double[] solutionEquation(PointForSpline pt1, PointForSpline pt2, PointForSpline pt3) {
+        double x1 = pt1.getCoordParameter();
+        double x2 = pt2.getCoordParameter();
+        double x3 = pt3.getCoordParameter();
+
         RealMatrix matrix = MatrixUtils.createRealMatrix(new double[][]{
                 {Math.pow(x1, 3), Math.pow(x1, 2), x1, 1},
                 {Math.pow(x2, 3), Math.pow(x2, 2), x2, 1},
                 {Math.pow(x3, 3), Math.pow(x3, 2), x3, 1},
-                { 3 * x1*x1, 2 * x1, 1, 0},
+                {3 * x1 * x1, 2 * x1, 1, 0},
+             //   {6 * x2, 4, 0, 0}
         });
         RealVector constants = new ArrayRealVector(
-                new double[] {y1, y2, y3, firstPoint.getFirstDiff()});
+                new double[]{pt1.getVal(), pt2.getVal(), pt3.getVal(), pt1.getFirstDiff()});
         LUDecomposition luDecomposition = new LUDecomposition(matrix);
         RealVector solution = luDecomposition.getSolver().solve(constants);
-        double[] arrCoeff = solution.toArray();
-        this.a =  arrCoeff[0];
-        this.b =  arrCoeff[1];
-        this.c =  arrCoeff[2];
-        this.d =  arrCoeff[3];
-    }
-    public SetOfPoints getPointsArr(){
-        SetOfPoints res = new SetOfPoints();
-        double length = thirdPoint.getX() - secondPoint.getX();
-        double x = secondPoint.getX();
-        for (int i = 0; i < length; i++){
-            x++;
-            res.addFirstSet(new PointForSpline(x, a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d));
-        }
-        length = secondPoint.getX() - firstPoint.getX();
-        x = firstPoint.getX();
-        for (int i = 0; i < length; i++){
-            x++;
-            res.addSecondSet(new PointForSpline(x, a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d));
-        }
-        return res;
+        double[] res = solution.toArray();
+        pt2.setFirstDiff(
+                3 * x2 * x2 * res[0] + 2 * x2  * res[1] + res[2]
+        );
+        pt3.setSecondDiff(
+                6 * x3  * res[0] + 4  * res[1]
+        );
+        return solution.toArray();
+
     }
 }
